7ae6071d995d45ecf35bc46d51a5401ed9a51a77
raul <raulochoa3005@gmail.com>
2022-04-13 11:46:22 -0430

    Añadí el index.html con links al resto de documentos

---
+++ descuentos.js
@@ -0,0 +1,42 @@
+const coupons = [
+    "Viaje70",
+    "Black Friday",
+    "Mi prrimera compra",
+];
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const InputCoupon = document.getElementById("InputCoupon");
+    const couponValue = InputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]: // Viaje70
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 50; //Black Friday
+        break;
+        case coupons[2]: //Mi primera compra
+            descuento = 70;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+
+    resultP.innerText = "El precio con descuento son $" + precioConDescuento;
+
+}
---
+++ salarios.js
@@ -0,0 +1,63 @@
+const colombia = [];
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+colombia.push({
+    name: "Carlos",
+    salary: 1150,
+});colombia.push({
+    name: "Raúl",
+    salary: 1300,
+});colombia.push({
+    name: "Ysabel",
+    salary: 500,
+});colombia.push({
+    name: "Victor",
+    salary: 1700,
+});colombia.push({
+    name: "Arturo",
+    salary: 2000,
+});colombia.push({
+    name: "Genesis",
+    salary: 1600,
+});colombia.push({
+    name: "Ovimer",
+    salary: 100,
+});colombia.push({
+    name: "Diego",
+    salary: 1300,
+});colombia.push({
+    name: "Adnre",
+    salary: 1550,
+});colombia.push({
+    name: "Liusbel",
+    salary: 2000,
+});colombia.push({
+    name: "Jose",
+    salary: 1300,
+});colombia.push({
+    name: "Abel",
+    salary: 2300,
+});colombia.push({
+    name: "Maria",
+    salary: 1000,
+});colombia.push({
+    name: "Angie",
+    salary: 1200,
+});colombia.push({
+    name: "Sophia",
+    salary: 1750,
+});colombia.push({
+    name: "Carolina",
+    salary: 1640,
+});colombia.push({
+    name: "Ricardo",
+    salary: 1300,
+});colombia.push({
+    name: "Merlinda",
+    salary: 1600,
+});colombia.push({
+    name: "Ovidio",
+    salary: 1700,
+});
