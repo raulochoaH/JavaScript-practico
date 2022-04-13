3b64bc0065550918129f24a8dfa55ece113ee98d
raulochoaH <103056399+raulochoaH@users.noreply.github.com>
2022-04-13 12:24:39 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # JavaScript-practico
 
 ..
@@ -14,4 +15,29 @@
 - Primer paso: definir las fórmulas
 - Segundo paso: implementar las fórmulas en JavaScript
 - Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Practica #3: promedio. mediana y moda
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+=======
+# JavaScript-practico
+
+..
+
+## Practica #1: fuguras geométricas
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+- Cuarto paso: integrar JS con HTML
+
+## Practica #2: porcentajes y descuentos
+
+- Primer paso: definir las fórmulas
+- Segundo paso: implementar las fórmulas en JavaScript
+- Tercer paso: crear funciones
+>>>>>>> b898a3dc632007140fc00c4a569931408e1006b4
 - Cuarto paso: integrar JS con HTML
--- descuentos.js
+++ descuentos.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const coupons = [
     "Viaje70",
     "Black Friday",
@@ -39,4 +40,47 @@ function onClickButtonPriceDiscount() {
 
     resultP.innerText = "El precio con descuento son $" + precioConDescuento;
 
+=======
+const coupons = [
+    "Viaje70",
+    "Black Friday",
+    "Mi prrimera compra",
+];
+
+function calcularPrecioConDescuento(precio, descuento){
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+    const InputCoupon = document.getElementById("InputCoupon");
+    const couponValue = InputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case coupons[0]: // Viaje70
+            descuento = 15;
+        break;
+        case coupons[1]:
+            descuento = 50; //Black Friday
+        break;
+        case coupons[2]: //Mi primera compra
+            descuento = 70;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+
+    const resultP = document.getElementById("ResultP");
+
+    resultP.innerText = "El precio con descuento son $" + precioConDescuento;
+
+>>>>>>> b898a3dc632007140fc00c4a569931408e1006b4
 }
